3e0514f752ff3df00abb2739fa4bfd53b3525512
keila cressman <kcressman@muhlenberg.edu>
2022-05-10 20:58:49 -0400

    added overlay feature

--- src/app/component/main/main.component.ts
+++ src/app/component/main/main.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+//create a list for photos
+//create a list to match etsy link to photo number in list
+  //apple(2) search 1st index of links list for etsy link
+//if a row has less pictures than the 3 rows add 
+  //the new picture there
+//create a way an overlay is attached to each pic automatically
   constructor() { }
 
   ngOnInit(): void {
