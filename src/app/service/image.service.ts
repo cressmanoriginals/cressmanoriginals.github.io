e8127f2e82af1714c406be2142f5d7276a7b9639
keila cressman <kcressman@muhlenberg.edu>
2022-05-19 18:33:14 -0400

    fixed photos

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -15,7 +15,7 @@ export class ImageService {
     { image: "./assets/paintings/cancer.jpg" },
     { image: "./assets/paintings/cheese.jpg" },
     { image: "./assets/paintings/coffee.jpg" },
-    { image: "./assets/paintings/crab_on_beach.jpg" },
+    { image: "./assets/paintings/beach_crab.jpg" },
     { image: "./assets/paintings/dragonfly.jpg" },
     { image: "./assets/paintings/empty_vessel.jpg" },
     { image: "./assets/paintings/face.jpg" },
@@ -26,7 +26,7 @@ export class ImageService {
     { image: "./assets/paintings/humming_bird.jpg" },
     { image: "./assets/paintings/icicles.jpg" },
     { image: "./assets/paintings/ladybug.jpg" },
-    { image: "./assets/paintings/leaf_fossil.jpg" },
+    { image: "./assets/paintings/boho_leaf.jpg" },
     { image: "./assets/paintings/lion.jpg" },
     { image: "./assets/paintings/orange.jpg" },
     { image: "./assets/paintings/orange_swirl.jpg" },
@@ -38,8 +38,8 @@ export class ImageService {
     { image: "./assets/paintings/seashells.jpg" },
     { image: "./assets/paintings/starfish.jpg" },
     { image: "./assets/paintings/sun.jpg" },
-    { image: "./assets/paintings/sunset.jpg" },
-    { image: "./assets/paintings/window.jpg" },
+    { image: "./assets/paintings/boho_sunset.jpg" },
+    { image: "./assets/paintings/window.jpg" }
   ]
   constructor() { }
 
